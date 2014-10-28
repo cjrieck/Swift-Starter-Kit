6a3faf8e030de5d99b507dea8f417f9f41d38bff
Clayton Rieck <crieck@CRieck.local>
2014-10-28 11:56:00 -0400

    added basic tableview

--- App/Swift Starter Kit/ViewController.swift
+++ App/Swift Starter Kit/ViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    weak var classTableView: UITableView!
     
     init(title: String!) {
         super.init(nibName: nil, bundle: nil)
@@ -20,21 +22,55 @@ class ViewController: UIViewController {
     }
     
     override func loadView() {
-        // add code during view init
+        self.view = UIView(frame: UIScreen.mainScreen().bounds)
+        self.view.backgroundColor = UIColor.whiteColor()
+        self.view.autoresizesSubviews = true
         
+        // add code during view init
+        let viewHeight = CGRectGetHeight(self.view.frame)
+        let viewWidth = CGRectGetWidth(self.view.frame)
+        
+        var classTableView = UITableView(frame: self.view.frame, style: UITableViewStyle.Plain)
+        classTableView.setTranslatesAutoresizingMaskIntoConstraints(false)
+        classTableView.autoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight
+        classTableView.delegate = self
+        classTableView.dataSource = self
+        self.view.addSubview(classTableView)
+        self.classTableView = classTableView
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Edit, target: self, action: "editTextField")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: UITableView Delegate
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        println("selected row \(indexPath.row)")
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10 // replace with array length
+    }
+    
+    // MARK: UITableView Datasource
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        cell.textLabel.text = "Cell \(indexPath.row)"
+        return cell
+    }
 }
 
