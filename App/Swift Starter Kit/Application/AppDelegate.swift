fb759367dc0ed5cc29c88cac7fca85e7e9f2573a
cjrieck <cjrieck123@gmail.com>
2014-10-30 09:17:18 -0400

    document app delegate

--- App/Swift Starter Kit/Application/AppDelegate.swift
+++ App/Swift Starter Kit/Application/AppDelegate.swift
@@ -10,24 +10,31 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    // app delegate properties MUST be optional unless it has a value during initialization or is lazily resolved
     var window: UIWindow?
     var navController: UINavigationController?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // create a window the size of the screen of the device via the mainScreen singleton
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         
+        // create basic view controller to be the root
         var rootVC = ViewController(title: "My Title")
         rootVC.view.backgroundColor = UIColor.whiteColor()
         
+        // set up nav controller to control app navigation with previously made view controller as its root
         navController = UINavigationController(rootViewController: rootVC)
         
-        window!.rootViewController = navController
+        // set the nav controller as the rootViewController on the window
+        window!.rootViewController = navController // be careful when force unwrapping an optional. Only do if you're 100% sure there's a value associated with the object
         window!.makeKeyAndVisible()
         return true
     }
 
+    // app delegate state/lifecycle methods. ere just for reference. Usually, we'd delete if not being implemented
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
